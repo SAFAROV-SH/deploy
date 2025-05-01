6ec0e6e2bb2c3f1fcfe3070201ce296e4d020cb8
Shohijahon <safarovdeveloper@gmail.com>
2025-05-01 22:57:16 +0500

    changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -103,11 +103,11 @@ export default function PrizeWheel() {
       <Header />
       <div className="flex flex-col items-center justify-center w-full max-w-full px-4 py-8 relative">
         <div className="relative w-full aspect-square max-w-xs mx-auto">
-          {/* Top indicator triangle marker */}
+          {/* Top indicator triangle marker - with corrected direction */}
           <div className="absolute w-8 h-10 top-0 left-1/2 transform -translate-x-1/2 -translate-y-4 z-20">
             <div className="w-0 h-0 
-                           border-l-[16px] border-r-[16px] border-b-[28px] 
-                           border-l-transparent border-r-transparent border-b-red-600
+                           border-l-[16px] border-r-[16px] border-t-[28px] 
+                           border-l-transparent border-r-transparent border-t-red-600
                            mx-auto filter drop-shadow-lg">
             </div>
           </div>
@@ -125,9 +125,6 @@ export default function PrizeWheel() {
                 transition: spinning ? `transform ${spinning ? '5s' : '0s'} cubic-bezier(0.2, 0.8, 0.3, 1)` : 'none'
               }}
             >
-              {/* Golden Circle Border */}
-              <circle cx="150" cy="150" r="149" fill="none" stroke="#fff" strokeWidth="8" />
-              
               {/* Wheel Segments */}
               {segments.map((segment, index) => (
                 <g key={index}>
@@ -158,17 +155,8 @@ export default function PrizeWheel() {
             </svg>
           </div>
           
-          {/* Outer blue border */}
+          {/* Outer blue border only */}
           <div className="absolute inset-0 rounded-full border-8 border-blue-600 pointer-events-none"></div>
-          
-          {/* Bottom triangle marker (indicator for winning segment) */}
-          {/* <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-2 z-20">
-            <div className="w-0 h-0 
-                           border-l-[14px] border-r-[14px] border-t-[20px] 
-                           border-l-transparent border-r-transparent border-t-red-600
-                           filter drop-shadow-lg">
-            </div>
-          </div> */}
         </div>
         
         <div className="mt-8 flex flex-col items-center w-full">
